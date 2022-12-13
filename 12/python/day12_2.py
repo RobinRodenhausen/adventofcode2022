75a1a8dc5d7bc898d436028e250df3c8b9aac600
Robin Rodenhausen <RodenhausenR@telekom.de>
2022-12-13 12:25:15 +0100

    Day 12

---
+++ 12/python/day12_2.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+from dataclasses import dataclass
+
+@dataclass
+class Coord:
+    x: int
+    y: int
+
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    def copy(self):
+        return Coord(self.x, self.y)
+
+    def __eq__(self, __o: object) -> bool:
+        return self.x == __o.x and self.y == __o.y
+
+    def __hash__(self) -> int:
+        return hash(('x', self.x, 'y', self.y))
+
+class Elevation:
+    def __init__(self, lines) -> None:
+        self.x_size = len(lines[0])
+        self.y_size = len(lines)
+        self.elevation_map = [[-1 for x in range(self.x_size)] for y in range(self.y_size)]
+
+        self.start = []
+
+        for l_index, line in enumerate(lines):
+            for c_index, char in enumerate(line):
+                if char == 'S':
+                    self.start.append(Coord(c_index, l_index))
+                    char = 'a'
+                elif char == 'E':
+                    self.end = Coord(c_index, l_index)
+                    char = 'z'
+                elif char == 'a':
+                    self.start.append(Coord(c_index, l_index))
+                self.elevation_map[l_index][c_index] = ord(char) - 97
+
+        self.total_steps = 0
+        self.possible_locations = [self.start]
+
+    def check_right(self, coord: Coord):
+        if coord.x < self.x_size - 1:
+            if self.elevation_map[coord.y][coord.x] + 1 >= self.elevation_map[coord.y][coord.x + 1]:
+                return True
+        return False
+
+    def check_left(self, coord: Coord):
+        if coord.x > 0:
+            if self.elevation_map[coord.y][coord.x] + 1 >= self.elevation_map[coord.y][coord.x - 1]:
+                return True
+        return False
+
+    def check_down(self, coord: Coord):
+        if coord.y < self.y_size - 1:
+            if self.elevation_map[coord.y][coord.x] + 1 >= self.elevation_map[coord.y + 1][coord.x]:
+                return True
+        return False
+
+    def check_up(self, coord: Coord):
+        if coord.y > 0:
+            if self.elevation_map[coord.y][coord.x] + 1 >= self.elevation_map[coord.y - 1][coord.x]:
+                return True
+        return False
+
+    def step(self):
+        self.total_steps += 1
+        print(f'Step {self.total_steps}')
+        self.possible_locations.append([])
+        for coord in self.possible_locations[self.total_steps - 1]:
+            if self.check_right(coord):
+                new = Coord(coord.x + 1, coord.y)
+                if not self.check_already_visited(new):
+                    self.possible_locations[self.total_steps].append(new)
+            if self.check_left(coord):
+                new = Coord(coord.x - 1, coord.y)
+                if not self.check_already_visited(new):
+                    self.possible_locations[self.total_steps].append(new)
+            if self.check_up(coord):
+                new = Coord(coord.x, coord.y - 1)
+                if not self.check_already_visited(new):
+                    self.possible_locations[self.total_steps].append(new)
+            if self.check_down(coord):
+                new = Coord(coord.x, coord.y + 1)
+                if not self.check_already_visited(new):
+                    self.possible_locations[self.total_steps].append(new)
+
+    def check_already_visited(self, check_loc):
+        for locations in self.possible_locations:
+            if check_loc in locations:
+                return True
+        return False
+
+    def get_shortest_step(self):
+        while not self.end in self.possible_locations[self.total_steps]:
+            #print(f'Ok {i} TS: {self.total_steps}')
+            #print(self.possible_locations[self.total_steps])
+            #print(self.end)
+            self.step()
+        return self.total_steps
+
+
+if __name__ == '__main__':
+    with open('12/input', 'r') as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+
+    e = Elevation(lines)
+    print(e.get_shortest_step())
--- README.md
+++ README.md
@@ -6,17 +6,18 @@
 
 ## Challanges
 
-| Day | Challange Link                                                 |
-| --- | -------------------------------------------------------------- |
-| 1   | [Calorie Counting](https://adventofcode.com/2022/day/1)        |
-| 2   | [Rock Paper Scissors](https://adventofcode.com/2022/day/2)     |
-| 3   | [Rucksack Reorganization](https://adventofcode.com/2022/day/3) |
-| 4   | [Camp Cleanup](https://adventofcode.com/2022/day/4)            |
-| 5   | [Supply Stacks](https://adventofcode.com/2022/day/5)           |
-| 6   | [Tuning Trouble](https://adventofcode.com/2022/day/6)          |
-| 7   | [No Space Left On Device](https://adventofcode.com/2022/day/7) |
-| 8   | [Treetop Tree House](https://adventofcode.com/2022/day/8)      |
-| 9   | [Rope Bridge](https://adventofcode.com/2022/day/9)             |
-| 10  | [Cathode-Ray Tube](https://adventofcode.com/2022/day/10)       |
-| 11  | [Monkey in the Middle](https://adventofcode.com/2022/day/11)   |
-| 12  | []()                                                           |
+| Day | Challange Link                                                  |
+| --- | --------------------------------------------------------------- |
+| 1   | [Calorie Counting](https://adventofcode.com/2022/day/1)         |
+| 2   | [Rock Paper Scissors](https://adventofcode.com/2022/day/2)      |
+| 3   | [Rucksack Reorganization](https://adventofcode.com/2022/day/3)  |
+| 4   | [Camp Cleanup](https://adventofcode.com/2022/day/4)             |
+| 5   | [Supply Stacks](https://adventofcode.com/2022/day/5)            |
+| 6   | [Tuning Trouble](https://adventofcode.com/2022/day/6)           |
+| 7   | [No Space Left On Device](https://adventofcode.com/2022/day/7)  |
+| 8   | [Treetop Tree House](https://adventofcode.com/2022/day/8)       |
+| 9   | [Rope Bridge](https://adventofcode.com/2022/day/9)              |
+| 10  | [Cathode-Ray Tube](https://adventofcode.com/2022/day/10)        |
+| 11  | [Monkey in the Middle](https://adventofcode.com/2022/day/11)    |
+| 12  | [Hill Climbing Algorithm](https://adventofcode.com/2022/day/12) |
+| 13  | []()                                                            |
